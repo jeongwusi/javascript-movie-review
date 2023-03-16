411b45e4f6e25245b4c949cd9cea523922e1656c
jeongwusi <90178210+wusi-hub@users.noreply.github.com>
2023-03-16 20:55:45 +0900

    호호호

--- src/assets/images.ts
+++
@@ -1,9 +0,0 @@
-import './logo.png';
-import './search_button.png';
-import './star_filled.png';
-
-export const IMAGES = {
-  LOGO: 'logo.png',
-  SEARCH_BUTTON: 'search_button.png',
-  STAR_FILLED: 'star_filled.png',
-};
