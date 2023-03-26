461368cb68a3fcb2b0fc9d996b385b840b02b8e4
jeongwusi <jeongwusi@gmail.com>
2023-03-27 06:56:05 +0900

    배포

--- src/domains/getAPI.ts
+++ src/domains/getAPI.ts
@@ -4,13 +4,13 @@ const SEARCH_API_PATH = '3/search/movie';
 
 const getAPI = {
   popularMovie: (page: number) => {
-    return `${BASE_API_DOMAIN}/${POPULAR_API_PATH}?api_key=${process.env.API_KEY}&language=ko-KR&page=${page}`;
+    return `${BASE_API_DOMAIN}/${POPULAR_API_PATH}?api_key=7346e1b315e7e7c5dc1e70459156cce2&language=ko-KR&page=${page}`;
   },
   searchMovie: (keyword: string, page: number) =>
-    `${BASE_API_DOMAIN}/${SEARCH_API_PATH}?api_key=${process.env.API_KEY}&language=ko-KR&query=${keyword}&page=${page}`,
+    `${BASE_API_DOMAIN}/${SEARCH_API_PATH}?api_key=7346e1b315e7e7c5dc1e70459156cce2&language=ko-KR&query=${keyword}&page=${page}`,
 
   detailMovie: (movieId: number) =>
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}&language=ko-KR`,
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=7346e1b315e7e7c5dc1e70459156cce2&language=ko-KR`,
 };
 
 export default getAPI;
